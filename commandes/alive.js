10cac90938fd75e327d6d5e790f78edbfd9c5bd1
Popkid <popkidtechs@gmail.com>
2025-01-03 13:33:26 +0300

    ⭕POP KID⭕

--- commandes/alive.js
+++ commandes/alive.js
@@ -10,7 +10,7 @@ zokou(
     // URLs and configurations
     const fullImageUrl = "https://i.ibb.co/n6rw805/694affc7ca5a5fb0cb58c2b4533f962d.jpg"; // Full image URL
     const smallThumbnailUrl = "https://i.ibb.co/n6rw805/694affc7ca5a5fb0cb58c2b4533f962d.jpg"; // Small thumbnail URL
-    const randomAudio = "https://files.catbox.moe/i6stvh.m4a"; // Voice note URL
+    const randomAudio = "https://files.catbox.moe/wdap4t.mp3"; // Voice note URL
     const sourceUrl = "https://whatsapp.com/channel/0029VadQrNI8KMqo79BiHr3lf"; // Channel link
     const contactName = commandeOptions?.ms?.pushName || "Unknown Contact"; // Sender's name or "Unknown Contact"
 
@@ -57,7 +57,7 @@ zokou(
     // URLs and configurations
     const fullImageUrl = "https://i.ibb.co/n6rw805/694affc7ca5a5fb0cb58c2b4533f962d.jpg"; // Full image URL
     const smallThumbnailUrl = "https://i.ibb.co/n6rw805/694affc7ca5a5fb0cb58c2b4533f962d.jpg"; // Small thumbnail URL
-    const randomAudio = "https://files.catbox.moe/i6stvh.m4a"; // Voice note URL
+    const randomAudio = "https://files.catbox.moe/wdap4t.mp3"; // Voice note URL
     const sourceUrl = "https://whatsapp.com/channel/0029VadQrNI8KMqo79BiHr3l"; // Channel link
     const contactName = commandeOptions?.ms?.pushName || "Unknown Contact"; // Sender's name or "Unknown Contact"
 
