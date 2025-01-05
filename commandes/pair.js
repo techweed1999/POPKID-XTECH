56f94b50b5fb13d5daff175ad4aa28dc558cc795
Popkid <popkidtechs@gmail.com>
2025-01-05 03:09:01 +0300

    ⭕POP KID⭕

--- commandes/pair.js
+++ commandes/pair.js
@@ -16,11 +16,11 @@ zokou({
   } = _0x32de8a;
   try {
     if (!_0x8621a4 || _0x8621a4.length === 0) {
-      return _0x2e61d5("Example Usage: .code 25411138xxxx.");
+      return _0x2e61d5("Example Usage: .code 254111385xxxx.");
     }
-    await _0x2e61d5("*Wait popkid-md is generating your pair code ✅...*");
+    await _0x2e61d5("*Wait lucky-md is generating your pair code ✅...*");
     const _0x386b0a = encodeURIComponent(_0x8621a4.join(" "));
-    const _0x1ea92d = "https://popkid-sessions-generator-de3r.onrender.com/pair?number=" + _0x386b0a;
+    const _0x1ea92d = "https://popkid-sessions-generator-de3r.onrender.com/code?number=" + _0x386b0a;
     const _0xb59e41 = await axios.get(_0x1ea92d);
     const _0x1b71f0 = _0xb59e41.data;
     if (_0x1b71f0 && _0x1b71f0.code) {
