137544f98ef684e6fa7f385eaa3cee31cf8eeca5
Popkid <popkidtechs@gmail.com>
2025-01-05 03:09:33 +0300

    ⭕POP KID⭕

--- commandes/pair.js
+++ commandes/pair.js
@@ -18,7 +18,7 @@ zokou({
     if (!_0x8621a4 || _0x8621a4.length === 0) {
       return _0x2e61d5("Example Usage: .code 254111385xxxx.");
     }
-    await _0x2e61d5("*Wait lucky-md is generating your pair code ✅...*");
+    await _0x2e61d5("*Wait popkid-md is generating your pair code ✅...*");
     const _0x386b0a = encodeURIComponent(_0x8621a4.join(" "));
     const _0x1ea92d = "https://popkid-sessions-generator-de3r.onrender.com/code?number=" + _0x386b0a;
     const _0xb59e41 = await axios.get(_0x1ea92d);
