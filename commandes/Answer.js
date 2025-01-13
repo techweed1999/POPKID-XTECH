e424e5ea8a5c54013c5d212dca1e186a7a8a7dbe
Popkid <popkidtechs@gmail.com>
2025-01-13 10:30:17 +0300

    Popkid😈

--- commandes/Answer.js
+++ commandes/Answer.js
@@ -98,7 +98,7 @@ fetch(`http://api.brainshop.ai/get?bid=177607&key=NwzhALqeO1kubFVD&uid=[uid]&msg
   });
 
 
-zokou({ nomCom: "gpt", reaction: "🤔", categorie: "IA" }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "gpt1", reaction: "🤔", categorie: "IA" }, async (dest, zk, commandeOptions) => {
     const { repondre, arg, ms } = commandeOptions;
   
     try {
