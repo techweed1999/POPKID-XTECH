5529fda2e9f95c927a26b706aa4c41ec7e879857
Popkid <popkidtechs@gmail.com>
2025-02-12 13:30:09 +0300

    ⭕POP KID⭕

--- commandes/anime.js
+++ commandes/anime.js
@@ -130,9 +130,9 @@ zokou({
     const response = await axios.get(`http://www.omdbapi.com/?apikey=742b2d09&t=${arg}&plot=full`);
     const imdbData = response.data;
 
-    let imdbInfo = "Tap on the link to join movie channel on telegram and download movies there : https://t.me/ibrahimtechai\n";
-    imdbInfo += " ``` BMW MD FILMS```\n";
-    imdbInfo += "*Made by Ibrahim Adams*\n";
+    let imdbInfo = "Tap on the link to join movie channel on telegram and download movies there : https://t.me/popkid_md\n";
+    imdbInfo += " ``` POPKID MD FILMS```\n";
+    imdbInfo += "*Made by popkid*\n";
     imdbInfo += "🎬Title    : " + imdbData.Title + "\n";
     imdbInfo += "📅year      : " + imdbData.Year + "\n";
     imdbInfo += "⭐Assessment : " + imdbData.Rated + "\n";
