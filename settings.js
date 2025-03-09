423cfcfca6fb62f2de92b6be947319fd53e6a561
Popkid-254 <popkidtechs@gmail.com>
2025-03-09 18:34:19 +0300

    ⭕POP KID⭕

---
+++ settings.js
@@ -0,0 +1,121 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "POPKID-XTECH~tw4QADrb#3vIeHvoICLX9uIJR1B52ygD5VfJSByWSrGcpBNQAn-c",
+// add your Session Id ,session id starts with MALVIN-XD~
+
+PREFIX: process.env.PREFIX || ".",
+// add your prifix for bot
+
+BOT_NAME: process.env.BOT_NAME || "✦popkid✦",
+// add bot name here for menu
+
+CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+// make this true for custum emoji react   
+
+STICKER_NAME: process.env.STICKER_NAME || "✦POPKIF-XTECH✦",
+// type sticker pack name  
+
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🔥,❤️‍🩹,❤️,🩷,🧡,💛,💚,💙,🩵,💜,🤎,🖤,🩶,🤍",
+// chose custom react emojis by yourself 
+
+DELETE_LINKS: process.env.DELETE_LINKS || "false",
+// automatic delete links witho remove member 
+
+OWNER_NUMBER: process.env.OWNER_NUMBER || "254111385747",
+// add your bot owner number
+
+OWNER_NAME: process.env.OWNER_NAME || "✦popkid🪀✦",
+// add bot owner name
+
+DESCRIPTION: process.env.DESCRIPTION || "> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ✦popkid✦*",
+// add bot owner name    
+
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/w5xf3f.jpg",
+// add img for alive msg
+
+MENU_IMG: process.env.MENU_IMG || "https://files.catbox.moe/w5xf3f.jpg",
+// dont change menu image
+
+LIVE_MSG: process.env.LIVE_MSG || "> ✦popkid xᴅ✦ ɪs sᴘᴀʀᴋɪɴɢ ᴀᴄᴛɪᴠᴇ ᴀɴᴅ ᴀʟɪᴠᴇ\n\n\nᴋᴇᴇᴘ ᴜsɪɴɢ ✦popkid xᴅ v2✦ ғʀᴏᴍ ɴᴇxᴜs ᴛᴇᴄʜ ɪɴᴄ⚡\n\n\n*© ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ - ᴍᴅ\n\n> ɢɪᴛʜᴜʙ :* github.com/Popkiddevs/POPKID-XTECH",
+// add alive msg here 
+
+READ_MESSAGE: process.env.READ_MESSAGE || "false",
+// Turn true or false for automatic read msgs
+
+AUTO_REACT: process.env.AUTO_REACT || "false",
+// make this true or false for auto react on all msgs
+
+ANTI_BAD: process.env.ANTI_BAD || "false",
+// false or true for anti bad words
+
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+// make true or false status auto seen
+
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+// make true if you want auto reply on status 
+
+AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+// make true if you want auto react on status (still testing)
+
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*🎉👀 Status Seen by ✦popkid🚀🔥✦*",
+// set the auto reply massage on status reply    
+
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
+
+ANTI_LINK: process.env.ANTI_LINK || "true",
+// make anti link true,false for groups 
+
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
+// make true for send automatic voices
+
+AUTO_STICKER: process.env.AUTO_STICKER || "false",
+// make true for automatic stickers 
+
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
+// make true or false automatic text reply 
+
+HEART_REACT: process.env.HEART_REACT || "false",
+// make this true or false for heart reactions only 
+
+OWNER_REACT: process.env.OWNER_REACT || "true",
+// make it true or fasle for only react on owner msg only 
+
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+// maks true for always online 
+
+PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+// make false if want private mod
+
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
+// true for automatic show typing   
+
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
+// make it true for auto recoding 
+
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+// make it true for fake recoding 
+
+FAKE_TYPING: process.env.FAKE_TYPING || "false",
+// make it true for fake typing
+
+READ_CMD: process.env.READ_CMD || "false",
+// true if want mark commands as read 
+
+DEV: process.env.DEV || "254111385747",
+//replace with your whatsapp number       
+ 
+ANTI_VV: process.env.ANTI_VV || "true",
+// true for anti once view 
+
+ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
+// change it to 'same' if you want to resend deleted message in same chat 
+
+CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
+// make it true for current status 
+};
