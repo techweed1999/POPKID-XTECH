90a6d084893c4dba6f4c0f0180cb71fa152b6567
Popkid-254 <popkidtechs@gmail.com>
2025-03-11 13:26:12 +0300

    ⭕POP KID⭕

---
+++ config.cjs
@@ -0,0 +1,33 @@
+// config.js
+const fs = require("fs");
+require("dotenv").config();
+
+const config = {
+  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  PREFIX: process.env.PREFIX || '.',
+  AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
+  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT !== undefined ? process.env.AUTO_STATUS_REACT === 'true' : false,
+  AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : false,
+  AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG !== undefined ? process.env.AUTO_STATUS_MSG === 'Popkid-MD' : " POPKID-MD",
+  AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
+  AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
+  AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
+  AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
+  ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
+  AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
+   /*auto block only for 212 */
+  AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
+  AUTO_BIO: process.env.AUTO_BIO !== undefined ? process.env.AUTO_BIO === 'true' : false,
+  CHAT_BOT: process.env.CHAT_BOT !== undefined ? process.env.CHAT_BOT === 'true' : false,
+  HEART_REACT: process.env.HEART_REACT !== undefined ? process.env.HEART_REACT === 'true' : false,
+  REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
+  NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
+  MODE: process.env.MODE || "private",
+  OWNER_NAME: process.env.OWNER_NAME || "©popkid",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "254111385747",
+  GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
+  WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
+};
+
+
+module.exports = config;
