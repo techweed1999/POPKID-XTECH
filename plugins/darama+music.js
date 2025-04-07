0a888450a03132e7eb5e2d962d53d54d2c11892a
Popkid-254 <popkidtechs@gmail.com>
2025-04-07 06:54:55 +0300

    Create darama+music.js

---
+++ plugins/darama+music.js
@@ -0,0 +1,9 @@
+//powered by popkid
+>>>> console ten 
+all logs to algorithm 5{
+enable html con - js   }
+copy stream path root -(jemini)
+0875857popkid $g env.key
+hunt from cracked version-----{https://github.com/popkiddevs/ZINJAR-MD}
+                               REMOTE::::;;;;;;508
+      ROOT ---PSIPPHHY GIT.ENV
