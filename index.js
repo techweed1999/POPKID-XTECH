117e133b6ee83cf9c94983b3d9653e1636f1dae7
Popkid-254 <popkidtechs@gmail.com>
2025-04-09 07:05:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+{running in jid5}
+    :popkid xtech 2025
+remote7: >>>>>>https:github.com/Popkiddevs/POPKID-PING-MD
 
+//EVERYTHING MADE BY POPKID DONT COPY
 
 import dotenv from 'dotenv';
 dotenv.config();
